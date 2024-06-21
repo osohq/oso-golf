a934f03d8c38f1ba57a08e012747fbc0aa0680ee
Valeri Karpov <val@karpov.io>
2024-06-21 07:34:14 -0400

    add another tell() test

--- test/tell.test.js
+++ test/tell.test.js
@@ -5,15 +5,30 @@ require('./setup');
 const Player = require('../db/player');
 const assert = require('assert');
 const connect = require('../db/connect');
-const { after, before, describe, it } = require('node:test');
+const { after, before, beforeEach, describe, it } = require('node:test');
 const oso = require('../oso');
 const sinon = require('sinon');
 const tell = require('../backend/tell');
 
 describe('tell', function() {
+  let player;
+
   before(async function() {
     await connect();
     await Player.deleteMany({});
+
+    player = await Player.create({
+      sessionId: 'test',
+      name: 'John Smith',
+      email: 'john@gmail.com',
+      contextFacts: []
+    });
+  });
+
+  beforeEach(async function() {
+    player = await Player.findById(player._id).orFail();
+    player.contextFacts = [];
+    await player.save();
   });
 
   after(async function() {
@@ -22,15 +37,8 @@ describe('tell', function() {
   });
 
   it('adds has_role facts to contextFacts', async function() {
-    const { _id, sessionId } = await Player.create({
-      sessionId: 'test',
-      name: 'John Smith',
-      email: 'john@gmail.com',
-      contextFacts: []
-    });
-
     await tell({
-      sessionId,
+      sessionId: player.sessionId,
       factType: 'role',
       userId: 'Alice',
       role: 'admin',
@@ -38,12 +46,28 @@ describe('tell', function() {
       resourceId: 'osohq/web'
     });
 
-    const player = await Player.findById(_id).orFail();
+    player = await Player.findById(player._id).orFail();
     assert.deepStrictEqual(player.contextFacts, [[
       'has_role',
       { type: 'User', id: 'Alice' },
       'admin',
       { type: 'Repository', id: 'osohq/web' }
+    ]]);
+  });
+
+  it('adds superadmin global role to contextFacts', async function() {
+    await tell({
+      sessionId: player.sessionId,
+      factType: 'role',
+      userId: 'Anthony',
+      role: 'superadmin'
+    });
+
+    player = await Player.findById(player._id).orFail();
+    assert.deepStrictEqual(player.contextFacts, [[
+      'has_role',
+      { type: 'User', id: 'Anthony' },
+      'superadmin'
     ]]);
   });
 });
