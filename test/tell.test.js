2dc8cf88bfa9397013fec08da97e9c1fdcb7f1df
Valeri Karpov <val@karpov.io>
2024-06-21 10:44:00 -0400

    add test coverage for remaining tell() code paths

--- test/tell.test.js
+++ test/tell.test.js
@@ -70,4 +70,58 @@ describe('tell', function() {
       'superadmin'
     ]]);
   });
+
+  it('adds has_default_role attribute to contextFacts', async function() {
+    await tell({
+      sessionId: player.sessionId,
+      factType: 'attribute',
+      attribute: 'has_default_role',
+      attributeValue: 'true',
+      resourceType: 'Organization',
+      resourceId: 'org1'
+    });
+
+    player = await Player.findById(player._id).orFail();
+    assert.deepStrictEqual(player.contextFacts, [[
+      'has_default_role',
+      { type: 'Organization', id: 'org1' },
+      'true'
+    ]]);
+  });
+
+  it('adds has_group attribute to contextFacts', async function() {
+    await tell({
+      sessionId: player.sessionId,
+      factType: 'attribute',
+      attribute: 'has_group',
+      attributeValue: 'group1',
+      resourceType: 'Project',
+      resourceId: 'proj1'
+    });
+
+    player = await Player.findById(player._id).orFail();
+    assert.deepStrictEqual(player.contextFacts, [[
+      'has_group',
+      { type: 'Project', id: 'proj1' },
+      { type: 'Group', id: 'group1' }
+    ]]);
+  });
+
+  it('adds generic attribute to contextFacts', async function() {
+    await tell({
+      sessionId: player.sessionId,
+      factType: 'attribute',
+      attribute: 'is_taco',
+      attributeValue: 'true',
+      resourceType: 'Repository',
+      resourceId: 'osohq/web'
+    });
+
+    player = await Player.findById(player._id).orFail();
+    assert.deepStrictEqual(player.contextFacts, [[
+      'is_taco',
+      { type: 'Repository', id: 'osohq/web' },
+      { type: 'Boolean', id: 'true' }
+    ]]);
+  });
 });
